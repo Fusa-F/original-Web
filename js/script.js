d2d12b8e3885c706d16f8040db0b7d5a622d40eb
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-03-06 21:47:20 +0900

    Responsive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 $(function(){
 
+    var contents = ["#top","#concept","#menu","#view","#reserve"];
+    var btn = ["#top-btn","#con-btn","#menu-btn","#view-btn","#res-btn"];
+
+    //btnEvent
+    function btnEvent(object, num){
+        var speed = 800;
+        $('html,body').animate({scrollTop: $(object).offset().top - num},speed);
+    }
+    $(btn[0]).click(function() {  
+        btnEvent(contents[0], 0);     
+    });
+    $(btn[1]).click(function() {   
+        btnEvent(contents[1], 160);  
+    });
+    $(btn[2]).click(function() {
+        btnEvent(contents[2], 100);  
+    });
+    $(btn[3]).click(function() {
+        btnEvent(contents[3], 0);       
+    });
+    $(btn[4]).click(function() { 
+        btnEvent(contents[4], 160);      
+    });
+
     //menu-slider
     $('.slider').slick({
         autoplay: false,
@@ -23,10 +47,18 @@ $(function(){
     //scrollEvent
     $(window).scroll(function (){
         var scroll = $(window).scrollTop();
-        var target = $(contents[i]).offset().top -70;
+        var target = $(contents[1]).offset().top;
         var docHeight = $(document).innerHeight();
         var windowHeight = $(window).innerHeight();
         var pageBottom = docHeight - windowHeight;   
+
+        $('.fadein,.section-title').each(function(){
+            var targetElement = $(this).offset().top;
+            if (scroll > targetElement - windowHeight +100){
+                $(this).css('opacity','1');
+                $(this).css('transform','translate(0,0)');
+            }
+        });
 
     });
 });
